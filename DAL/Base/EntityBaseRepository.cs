6879425977a11c107128c4eb084053f15244ee98
Tuan Anh Nguyen <anhntph19124@fpt.edu.vn>
2023-02-06 17:44:45 +0700

    Thêm BaseRepository trong thư mục Base ( BaseRepository này là các hàm CRUD dung chung cho tất cả các model) và e cũng làm mẫu cách sử dụng mấy cái file trong thư mục Base như nào mn làm theo và gọi vào api nhé

---
+++ DAL/Base/EntityBaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        public readonly DbContexts _contexts;
+        public EntityBaseRepository(DbContexts contexts)
+        {
+            _contexts = contexts;
+        }
+
+        public async Task<ActionResult<IEnumerable<T>>> AddAsync(T entity)
+        {
+            await _contexts.Set<T>().AddAsync(entity);
+            await _contexts.SaveChangesAsync();
+            return await _contexts.Set<T>().ToListAsync();
+           
+        }
+
+        public async Task<ActionResult<IEnumerable<T>>> GetAllAsync()
+        {
+            var result = await _contexts.Set<T>().ToListAsync();
+            return result;
+        }
+
+        public async Task<ActionResult<T>> GetOneAsync(int id)
+        {
+            var result = await _contexts.Set<T>().FirstOrDefaultAsync(p => p.Id == id);
+            return result;
+        }
+
+        public async Task<ActionResult<IEnumerable<T>>> RemoveAsync(int id)
+        {
+            var entity = await _contexts.Set<T>().FirstOrDefaultAsync(p => p.Id == id);
+            EntityEntry entityEntry = _contexts.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
+            await _contexts.SaveChangesAsync();
+            return await _contexts.Set<T>().ToListAsync();
+        }
+
+        public async Task<ActionResult<IEnumerable<T>>> UpdateAsync(T entity)
+        {
+            EntityEntry entityEntry = _contexts.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
+            await _contexts.SaveChangesAsync();
+            return await _contexts.Set<T>().ToListAsync();
+        }
+    }
+}
