1ec5608459c39337985784a6d1c140e92e707ecf
hà nguyễn <hanvph19236@fpt.edu.vn>
2023-02-11 16:29:50 +0700

    Ha

---
+++ CSharp5/Migrations/20230202095955_haha.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CSharp5.Migrations
+{
+    public partial class haha : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
