c43f67f4bb4d7e9f11a5e4f4bb145097598ce7a0
Tuan Anh Nguyen <anhntph19124@fpt.edu.vn>
2023-02-19 15:14:59 +0700

    using system.linq

--- CSharp5/Controllers/NguoiDungsController.cs
+++ CSharp5/Controllers/NguoiDungsController.cs
@@ -3,6 +3,7 @@ using DAL.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CSharp5.Controllers
